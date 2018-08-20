a4dab6be3c2f5d175533353034edbf26d5d97208
NVKPAVANKUMAR <npavankumar144@gmail.com>
2018-08-20 15:25:28 +0530

    first commit to serenity

--- src/test/java/TestRunner1.java
+++ src/test/java/TestRunner1.java
@@ -8,8 +8,15 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/features/Login.feature")
-public class TestRunner1 {
 
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/features/Login.feature",
+                glue = {"stepDefinitions"})
+public class TestRunner1 {
+   /* private JUnit4TestRunner jUnit4TestRunner;
+
+    @Test(description = "Runs Cucumber Feature")
+    public void feature(CucumberFeatureWrapper cucumberFeature) {
+        jUnit4TestRunner.run(TestRunner1.class);
+    }*/
 }
